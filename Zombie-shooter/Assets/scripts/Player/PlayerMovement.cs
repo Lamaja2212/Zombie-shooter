cda988068dc5d683aad3c92c15d53626c1bc71f5
Lamaja2212 <75688864+Lamaja2212@users.noreply.github.com>
2020-12-08 19:36:50 +0100

    Adding Movement and Turning to hte Player
    Adding newPlayerMovement Script where we have added movement and turning to the Player Game Object

---
+++ Zombie-shooter/Assets/scripts/Player/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+    Vector3 _movement;
+    Animator _anim;
+    Rigidbody _playerRb;
+    int _floorMask;
+    float camReyLength = 100f;
+    void Awake()
+    {
+        _floorMask = LayerMask.GetMask("Floor");
+        _anim = GetComponent<Animator>();
+        _playerRb = GetComponent<Rigidbody>();
+    }
+
+    
+    void fixedUpdate()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        Move(horizontalInput, verticalInput);
+        Turning();
+    }
+    void Move(float h, float v)
+    {
+        _movement.Set(h, 0f, v);
+        _movement = _movement.normalized * speed * Time.deltaTime;
+        _playerRb.MovePosition(transform.position + _movement);
+    }
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+        if(Physics.Raycast(camRay, out floorHit, camReyLength, _floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            _playerRb.MoveRotation(newRotation);
+        }
+    }
+}
